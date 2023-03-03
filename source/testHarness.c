d7794cb5c2c3c48b7810ecb38f9500ba67e962e8
Ev7 <33555329+E-v7@users.noreply.github.com>
2023-03-03 07:55:25 +0000

    Main

--- source/testHarness.c
+++ source/testHarness.c
@@ -7,24 +7,41 @@
 */
 
 #include <stdio.h>
+#define BITMAX 4
 
-void takeInput(char* str);
+void printBinary(unsigned long num);
 
 int main(void) {
-    char* userInput = (char*)malloc(20);
+    unsigned long currentNum = 0b0;
+    unsigned long maxCount = 0b1 << BITMAX - 1;
 
+    // Set maxCount to highest possible binary number up to BITMAX
+    for (int i = BITMAX - 2; i >= 0; i--){
+        maxCount |= 1 << i;
+    }
+
+    // Print out numbers 1 by 1
+    do {
+        printBinary(currentNum++);
+    } while (currentNum ^ maxCount);
+    printBinary(currentNum); // Print last number
 
     return 0;
 }
 
-void takeInput(char* str) {
-    char* ptr = NULL; // char pointer used to find and remove a new line character
-
-    while (str += getc(stdin) != '\n') {
-        if (strlen(str) + 1 == sizeof(str)) {
-            str = (char*)realloc(sizeof(str) * 2);
+/*
+    Function        : printBinary()
+    Description     : Use to take a decimal number then print in it's binary representation
+    Parameters      : unsigned long num - The decimal number to be printed as binary
+    Return Value(s) : None
+*/
+void printBinary(unsigned long num) {
+    for (int i = BITMAX - 1; i >= 0; i--){
+        if (num & 1 << i) {
+          printf("1");
+        } else {
+          printf("0");
         }
     }
-    str[strlen(str) + 1] = '\0';
-    str = (char*)realloc(sizeof(str) + 1);
+   printf("\n");
 }
